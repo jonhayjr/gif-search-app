4370af539cfba004b76c0b84ef1420d6584468b1
jonhayjr <jonhayjr@gmail.com>
2021-06-04 20:42:07 -0700

    Added basename to resolve 404 error in github

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename='/search/dogs'>
       <App />
     </HashRouter>
   </React.StrictMode>,
