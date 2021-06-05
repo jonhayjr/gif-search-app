e874fdd6336f0341de67845a4d5b0bc86e639310
jonhayjr <jonhayjr@gmail.com>
2021-06-05 10:23:53 -0700

    Updated hearder margins

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 const Header = ({title}) => {
     return (
-        <header className='mb-4' >
+        <header className='mb-5 mt-3' >
             <h1 className='display-3'>{title}</h1>
         </header>
     )
